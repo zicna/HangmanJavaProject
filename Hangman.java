99dfeeeb30042d33de6c03fd04e69fc6f044a7bc
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-04-06 16:11:15 -0500

    return random word from array of words

--- Hangman.java
+++ Hangman.java
@@ -1,5 +1,12 @@
 public class Hangman{
     public static void main(String[] args) {
+        System.out.println(randomWord());
+    }
+
+    public static String randomWord(){
+        String[] words = {"ant", "baboon", "badger", "bat", "bear","beaver", "camel", "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat", "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose", "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal", "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan", "tiger", "toad", "trout", "turkey","turtle", "weasel", "whale", "wolf", "wombat", "zebra"};
         
+        int randomNumber = (int)(Math.random() * words.length);
+        return words[randomNumber];
     }
 }
